1f261c3cac80e10a444864dc0eba8c3119937741
Ylzammar <123626400+Ylzammar@users.noreply.github.com>
2023-03-01 11:46:43 +0100

    ammar

--- liste.php
+++ liste.php
@@ -1,24 +1,22 @@
+
 <html>
 <body>
-<h1>Liste des films</h1>
-
-
+<h1>Insertion du film</h1>
 <?php
-//Lister le contenu de la table movies
+
+$TITRE= $_GET['titre'];
+$GENRE = $_GET['genre'];
+$ANNEE = $_GET['annee'];
+
+echo $TITRE." ".$GENRE." ".$ANNEE; 
 
 //1° - Connexion à la BDD
 $base = new PDO('mysql:host=localhost; dbname=id20205714_facnanterre', 'id20205714_ammar', 'A8jFE5)UTW5Li*^o');
 $base->exec("SET CHARACTER SET utf8");
-
+$sql = 'INSERT INTO movies VALUES(NULL,"'.$TITRE.'","'.$GENRE.'",'.$ANNEE.')';
 //2° - Préparation de requette et execution
-$retour = $base->query('SELECT * FROM movies;');
-
-//3° - Lecture du resultat de la requette
-while ($data = $retour->fetch()){
-echo $data['id']." ".$data['titre']." ".$data['genre']." ".$data['annee']."</br>";
-}
+$retour = $base->query($sql);
 
 ?>
-
 </body>
 </html>
