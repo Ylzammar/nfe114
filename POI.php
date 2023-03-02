0226be37ffd98250a9e0a3d302b0e5eedbdbf309
Ylzammar <123626400+Ylzammar@users.noreply.github.com>
2023-03-02 12:04:55 +0100

    n

--- POI.php
+++ POI.php
@@ -5,8 +5,9 @@ echo "lat\tlon\ttitle\tdescription\ticon\ticonSize\ticonOffset\n";
 echo "48.858205\t2.294359\tMoi\tMa Position\tOl_icon_blue_example.png\t24,24\t0,-24\n";
 
 //1° - Connexion à la BDD
-$base = new PDO('mysql:host=localhost; dbname=convergence', 'convergence', 'Convergence2020%1');
+$base = new PDO('mysql:host=localhost; dbname=id20205714_facnanterre', 'id20205714_ammar', 'A8jFE5)UTW5Li*^o');
 $base->exec("SET CHARACTER SET utf8");
+$sql = 'INSERT INTO equipement'; 
 
 //2° - Préparation de requette et execution
 $retour = $base->query('SELECT *, get_distance_metres(\'48.858205\', \'2.294359\', equi_lat, equi_long) 
