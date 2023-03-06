036c4822f9641b8002bf0602e7dcc3ddd095d93b
Ylzammar <123626400+Ylzammar@users.noreply.github.com>
2023-03-06 14:41:20 +0100

    test

--- POI.php
+++ POI.php
@@ -1,15 +1,18 @@
 <?php
 
+$LONG = $_GET['long'];
+$LAT = $_GET['lat'];
+
 //C'est le POI de l'utilisateur
 echo "lat\tlon\ttitle\tdescription\ticon\ticonSize\ticonOffset\n";
-echo "48.858205\t2.294359\tMoi\tMa Position\tOl_icon_blue_example.png\t24,24\t0,-24\n";
+echo "$LAT\t$LONG\tMoi\tMa Position\tOl_icon_blue_example.png\t24,24\t0,-24\n";
 
 //1° - Connexion à la BDD
 $base = new PDO('mysql:host=localhost; dbname=id20205714_facnanterre', 'id20205714_ammar', 'A8jFE5)UTW5Li*^o');
 $base->exec("SET CHARACTER SET utf8");
 
 //2° - Préparation de requette et execution
-$retour = $base->query('SELECT *, get_distance_metres(\'48.858205\', \'2.294359\', equi_lat, equi_long) 
+$retour = $base->query('SELECT *, get_distance_metres(\'$LAT\', \'$LONG\', equi_lat, equi_long) 
 AS proximite 
 FROM equipement 
 HAVING proximite < 1000 ORDER BY proximite ASC
